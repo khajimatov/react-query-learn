f949377199cf81c172d178a09b778f4b1cf7a0a0
khajimatov <for.official.bkh@gmail.com>
2022-12-14 17:29:37 +0600

    Added tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,68 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import Hmm from './hmm'
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createReactRouter,
+  createRouteConfig,
+} from '@tanstack/react-router'
+import axios from 'axios'
 
-const queryClient = new QueryClient()
+
+
+const rootRoute = createRouteConfig({
+  component: () => (
+    <>
+      <div>
+        <Link to="/hmm">Home</Link> <Link to="/items">Items</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  )
+})
+
+const indexRoute = rootRoute.createRoute({
+  path: '/',
+  component: Index,
+})
+
+const itemsRoute = rootRoute.createRoute({
+  path: '/items',
+  component: Items,
+})
+
+const routeConfig = rootRoute.addChildren([indexRoute, itemsRoute])
+
+const router = createReactRouter({ routeConfig })
 
 function App() {
+  return <QueryClientProvider client={new QueryClient()}><RouterProvider router={router} /><ReactQueryDevtools /></QueryClientProvider>
+}
 
+function Index() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        Hello World
-        <Hmm />
-      </div>
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    <div>
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
+
+function Items() {
+  const fetchItems = () => {
+    return axios.get('https://611a826e5710ca00173a1a6e.mockapi.io/items')
+  }
+  const { data, isError, error, refetch } = useQuery('items', fetchItems, { enabled: false, refetchOnWindowFocus: false})
+  return (
+
+    <>
+      <div>Hello from Items!</div>
+      <button onClick={() => refetch()}>LEZGO</button>
+      {data?.data.map((value: any) => console.log(value))}
+    </>
+
   )
 }
 
